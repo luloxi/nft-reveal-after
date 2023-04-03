845ed54b944f073e9343f13c7bf9f1c173d63ebf
Lulox <ligma@duck.com>
2023-04-03 18:32:34 -0300

    Using Counters for gas optimization

--- contracts/ELC.sol
+++ contracts/ELC.sol
@@ -5,16 +5,20 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 error ELC__NeedMoreETHSent();
 error ELC__MintingCompleted();
 
 contract ELC is ERC721, Ownable {
-    
-    uint256 private s_tokenCounter;
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
     uint256 public constant i_mintFee = 76000000000000000; //0.076 ETH;
     bool public revealed = false;
 
-    string public baseURI = "ipfs://QmTVKvYvc3Djq8RW4PRKWiz3dFUd4XfYcRYDcBoQdnAi4J/"; 
+    string public baseURI = "ipfs://QmTVKvYvc3Djq8RW4PRKWiz3dFUd4XfYcRYDcBoQdnAi4J/";
     // Must have a hidden.json file inside that folder, placeholder image and metadata until reveal
 
     constructor() ERC721("Eco Lions Club", "ELC") {}
@@ -33,11 +37,12 @@ contract ELC is ERC721, Ownable {
         if (msg.value < i_mintFee) {
             revert ELC__NeedMoreETHSent();
         }
-        if (s_tokenCounter > 49) {
+        if (totalSupply() >= 50) {
             revert ELC__MintingCompleted();
         }
-        s_tokenCounter = s_tokenCounter + 1;
-        _safeMint(msg.sender, s_tokenCounter);
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _safeMint(msg.sender, newTokenId);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -53,7 +58,11 @@ contract ELC is ERC721, Ownable {
     }
 
     function getTokenCounter() public view returns (uint256) {
-        return s_tokenCounter;
+        return _tokenIds.current();
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 
     function _baseURI() internal view override returns (string memory) {
